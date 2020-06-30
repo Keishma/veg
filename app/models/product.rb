42992261dbcd63850e6ad19106230946d96cf270
Keishma <kieran.l@yandex.com>
2020-06-30 22:52:57 +0100

    check

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
 	has_attached_file :photo, styles: {
 	thumb: '100x100>',
 	square: '200x200#',
-	medium: '300x300>'
+	medium: '400x400>'
 	}
 
 	# Validate the attached image is image/jpg, image/png, etc
